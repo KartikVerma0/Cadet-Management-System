82f16edd49b006aec0faf30b509f33aff180f186
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-15 20:09:08 +0530

    Initialised ANO/CTO model

---
+++ backend/models/ano_ctoSchema.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const ano_ctoSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    mobileNumber: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    nccWing: {
+        type: String,
+        required: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    }
+})
+
+const ANO_CTO = mongoose.model("ANO_CTO", ano_ctoSchema);
+
+export default ANO_CTO;
