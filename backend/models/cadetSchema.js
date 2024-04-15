dc8fab21477186e4145f6101dbfeb91944033eac
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-15 20:09:23 +0530

    Initialised Cadet model

---
+++ backend/models/cadetSchema.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const cadetSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    nationality: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    fatherName: {
+        type: String,
+        required: true
+    },
+    motherName: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    mobileNumber: {
+        type: Number,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    bloodGroup: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    nearestRailwayStation: {
+        type: String,
+        required: true
+    },
+    nearestPoliceStation: {
+        type: String,
+        required: true
+    },
+    identificationMark1: {
+        type: String,
+        required: true
+    },
+    identificationMark2: {
+        type: String,
+        required: true
+    },
+    nccUnit: {
+        type: String,
+        required: true
+    },
+    nccWing: {
+        type: String,
+        required: true
+    },
+    enrollmentNumber: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    rollNumber: {
+        type: Number,
+        required: true
+    },
+    academicYear: {
+        type: Number,
+        required: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    }
+})
+
+const Cadet = mongoose.model("Cadet", cadetSchema);
+
+export default Cadet;
