bf53918d05e3ad573260a7db89423c357ae8c132
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-17 14:43:24 +0530

    Update: Added vercel analytics component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import IndexPage from "./pages/indexPage/IndexPage"
 import SignUpRouterPage from "./pages/signUpRouterPage/SignUpRouterPage"
 import LoginPage from "./pages/loginPage/LoginPage"
 import SignUpPage from "./pages/signupPage/SignUpPage"
+import { Analytics } from "@vercel/analytics/react"
 
 import { Route, Routes } from "react-router-dom"
 
@@ -10,15 +11,15 @@ import "./App.css"
 function App() {
 
   return (
-
-    <Routes>
-      <Route path="/" element={<IndexPage />} />
-      <Route path="/signup" element={<SignUpRouterPage />} />
-      <Route path="/login/:role" element={<LoginPage />} />
-      <Route path="/signup/:role" element={<SignUpPage />} />
-    </Routes>
-
-
+    <>
+      <Analytics />
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/signup" element={<SignUpRouterPage />} />
+        <Route path="/login/:role" element={<LoginPage />} />
+        <Route path="/signup/:role" element={<SignUpPage />} />
+      </Routes>
+    </>
   )
 }
 
