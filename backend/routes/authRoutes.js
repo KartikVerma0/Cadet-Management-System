998245b36e1e16dde34c4fdd7ab2d6f36d812064
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-19 15:28:59 +0530

    Update: Added attendance id in cadet info and cadet id in attendance info

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -39,36 +39,50 @@ router.post("/signup/:role", async (req, res) => {
                 mobileNumber, email, bloodGroup, gender, nearestRailwayStation,
                 nearestPoliceStation, identificationMark1, identificationMark2,
                 nccUnit, nccWing, enrollmentNumber, department, rollNumber,
-                academicYear, hashedPassword
+                academicYear, hashedPassword, attendanceOverallDetailsId: 0
             })
 
-            await newCadet.save()
-            // make attendance document when account is approved
-            let newCadetAttendance = new Attendance({
-                role
+            let cadetId = newCadet._id;
+            var newCadetAttendance = new Attendance({
+                role,
+                userId: cadetId
             })
+            let attendanceOverallDetailsId = newCadetAttendance._id;
+            newCadet.attendanceOverallDetailsId = attendanceOverallDetailsId;
             await newCadetAttendance.save()
+
+            await newCadet.save()
+
+
+            // make attendance document when account is approved
         } else if (role === 'PROBATION') {
             let newProbationer = new Probationer({
                 name, nationality, dob, fatherName, motherName, address,
                 mobileNumber, email, bloodGroup, gender, nearestRailwayStation,
                 nearestPoliceStation, identificationMark1, identificationMark2,
                 nccWing, department, rollNumber,
-                academicYear, hashedPassword
+                academicYear, hashedPassword, attendanceOverallDetailsId: 0
             })
 
-            await newProbationer.save()
+            let newProbationerId = newProbationer._id;
+
             // make attendance document when account is approved
-            let newProbationerAttendance = new Attendance({
-                role
+            var newProbationerAttendance = new Attendance({
+                role,
+                userId: newProbationerId
             })
+            let attendanceOverallDetailsId = newProbationerAttendance._id;
+            newProbationer.attendanceOverallDetailsId = attendanceOverallDetailsId;
             await newProbationerAttendance.save()
+
+            await newProbationer.save()
+
         } else {
             return res.json({ success: false, message: "No matching roles found" })
         }
         return res.json({ success: true, message: "Successfully signed up" })
     } catch (e) {
-        return res.json({ success: false, message: "Problem signing up" })
+        return res.json({ success: false, message: `Problem signing up.\nError: ${e.message}` })
     }
 })
 
