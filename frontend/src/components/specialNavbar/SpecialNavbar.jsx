9413e8695a0e7d91c32492aec1afc6d414b70bd4
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-19 15:32:04 +0530

    Update: Added model component and replaced link component with button elements

--- frontend/src/components/specialNavbar/SpecialNavbar.jsx
+++ frontend/src/components/specialNavbar/SpecialNavbar.jsx
@@ -1,12 +1,26 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import Model from '../modelComponent/Model'
 import './SpecialNavbar.css'
 
+
+
 export default function SpecialNavbar() {
+    const [selectedButton, setSelectedButton] = useState("");
+
+    const handleClick = (data) => {
+        setSelectedButton(data);
+    }
+
     return (
-        <nav className='SpecialNavbar'>
-            <Link to="/create/event">Create an Event</Link>
-            <Link to="/create/poll">Create a Poll</Link>
-            <Link to="/create/notification">Create a Notification</Link>
-        </nav>
+        <>
+            <section className='SpecialNavbar'>
+                <button to="/create/event" onClick={() => handleClick("Event")}>Create an Event</button>
+                <button to="/create/poll" onClick={() => handleClick("Poll")}>Create a Poll</button>
+                <button to="/create/notification" onClick={() => handleClick("Notification")}>Create a Notification</button>
+            </section>
+            {selectedButton !== "" && <Model closeButtonHandler={setSelectedButton} topic={selectedButton} />}
+
+        </>
+
     )
 }
