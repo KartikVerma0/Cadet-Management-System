d946f1b08c8d830e803a2551ae5fc822f10c15ab
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-19 15:26:27 +0530

    Update: Added create Router

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import authRouter from './routes/authRoutes.js'
+import createRouter from './routes/createRouter.js'
 
 import cors from 'cors'
 
@@ -34,14 +35,9 @@ connectToDatabase()
 
 app.use(cors())
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next()
-// })
-
 
 app.use("/", authRouter)
+app.use("/create", createRouter)
 
 
 app.listen(port, () => {
