5730004579a6df42ebec31196f1d39bcf6672abc
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-21 00:07:41 +0530

    Initialised Auth Context

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from 'react'
+import PropTypes from 'prop-types'
+
+const AuthContext = createContext({})
+export default AuthContext
+
+export function AuthProvider({ children }) {
+    const [auth, setAuth] = useState({})
+    // console.log(auth)
+
+    return (
+        <AuthContext.Provider value={{ auth, setAuth }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
