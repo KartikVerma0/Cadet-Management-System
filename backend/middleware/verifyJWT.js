2b1b7e8bdc3681a8df5b3fdec4d1a854e7ce07d6
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-21 00:01:08 +0530

    Initialised verify JWT middleware

---
+++ backend/middleware/verifyJWT.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken'
+
+export const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    console.log("AuthHeader " + authHeader)
+    if (!authHeader) {
+        return res.sendStatus(401);
+    }
+    const token = authHeader.split(' ')[1]
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(403) //invalid token
+        req.user = decoded.name
+        next()
+    })
+}
