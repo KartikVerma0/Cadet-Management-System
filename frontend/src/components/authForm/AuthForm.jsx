20ec6594f922cf4a7fb67e9ae9efef08bf8f8687
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-21 00:06:34 +0530

    Update: Added adding authentication data in context logic

--- frontend/src/components/authForm/AuthForm.jsx
+++ frontend/src/components/authForm/AuthForm.jsx
@@ -1,7 +1,9 @@
 import { useForm } from 'react-hook-form'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { BACKEND_BASE_STRING } from '../../env'
 import axios from 'axios'
+import AuthContext from '../../context/AuthContext'
 
 import PropTypes from 'prop-types'
 import "./AuthForm.css"
@@ -10,11 +12,19 @@ export default function AuthForm({ role, authType }) {
     const { register, handleSubmit } = useForm()
     const [errMsg, setErrMsg] = useState('')
     const [successLogin, setSuccessLogin] = useState(false)
+    const { setAuth } = useContext(AuthContext);
     const submitHandler = async (data) => {
 
         try {
-            const response = await axios.post(`${BACKEND_BASE_STRING}/login/${role}`, data)
+            const response = await axios.post(`${BACKEND_BASE_STRING}/login/${role}`, data, {
+                withCredentials: true
+            })
             if (response.data.success) {
+                const accessToken = response.data.accessToken
+                // console.log(accessToken)
+                //const roles = response.data.roles
+                //add roles in setAuth later
+                setAuth({ ...data, accessToken })
                 setSuccessLogin(true)
                 setErrMsg('')
             } else {
@@ -43,6 +53,7 @@ export default function AuthForm({ role, authType }) {
             </form>
             {errMsg && <p className='errorMessage font-red'>{errMsg.toString()}</p>}
             {successLogin && <p className='successMessage font-green'>Successfully logged in</p>}
+            <Link to="/dashboard">dash</Link>
         </div>
     )
 }
