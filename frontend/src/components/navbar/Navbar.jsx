c3f00f89f31a0972689b1e1164cd5420f736f585
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-22 01:23:06 +0530

    Update: Added logout logic

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,7 +2,27 @@ import PropTypes from 'prop-types';
 import "./Navbar.css"
 import { Link } from 'react-router-dom';
 
+import axios from '../../api/axios.js'
+
+import useAuth from '../../hooks/useAuth.js';
+
 export default function Navbar({ loginType }) {
+
+    const { auth, setAuth } = useAuth()
+    const role = auth.role
+
+    const handleLogout = async () => {
+        try {
+            await axios.get(`/logout/${role}`, {
+                withCredentials: true
+            })
+            setAuth({})
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
     return (
         <nav className="Navbar">
             <div className="leftSection">
@@ -16,7 +36,7 @@ export default function Navbar({ loginType }) {
 
             {loginType === "login" && <Link to="/" className="NavLink">LOGIN</Link>}
             {loginType === "signup" && <Link to="/signup" className="NavLink">SIGNUP</Link>}
-            {loginType === "logout" && <Link to="/logout" className="NavLink">LOGOUT</Link>}
+            {loginType === "logout" && <button className="NavLink" onClick={handleLogout}>LOGOUT</button>}
 
         </nav >
     )
