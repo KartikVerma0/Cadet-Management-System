ad6d07a242c78e2b9c259c73b78c8d598f33ba48
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-22 01:16:22 +0530

    Fix: Refactored the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import SignUpRouterPage from "./pages/signUpRouterPage/SignUpRouterPage"
 import LoginPage from "./pages/loginPage/LoginPage"
 import SignUpPage from "./pages/signupPage/SignUpPage"
 import Dashboard from "./pages/dashboard/Dashboard"
+import RequireAuth from "./components/requireAuth/RequireAuth"
+
 import { Analytics } from "@vercel/analytics/react"
 
 import { Route, Routes } from "react-router-dom"
@@ -15,12 +17,19 @@ function App() {
     <>
       <Analytics />
       <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/signup" element={<SignUpRouterPage />} />
-        <Route path="/login/:role" element={<LoginPage />} />
-        <Route path="/signup/:role" element={<SignUpPage />} />
-        {/* if logged in then show dashboard */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path={"/"} >
+          <Route index element={<IndexPage />} />
+          <Route path="login/:role" element={<LoginPage />} />
+        </Route>
+        <Route path="/signup" >
+          <Route index={true} element={<SignUpRouterPage />} />
+          <Route path=":role" element={<SignUpPage />} />
+        </Route>
+
+        <Route element={<RequireAuth />}>
+          {/* if logged in then show dashboard */}
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </>
   )
