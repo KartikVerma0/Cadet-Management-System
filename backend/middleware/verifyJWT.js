5921277fe51a28b44a3856dbb34a5ba4f996c68b
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-24 23:09:57 +0530

    Update: Added permissions array in req object

--- backend/middleware/verifyJWT.js
+++ backend/middleware/verifyJWT.js
@@ -1,15 +1,15 @@
 import jwt from 'jsonwebtoken'
 
 export const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization']
+    const authHeader = req.headers.authorization || req.headers.Authorization
     if (!authHeader) {
         return res.sendStatus(401);
     }
     const token = authHeader.split(' ')[1]
-
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) return res.sendStatus(403) //invalid token
-        req.user = decoded.name
+        req.user = decoded.UserInfo.name
+        req.permissions = decoded.UserInfo.permissions
         next()
     })
 }
