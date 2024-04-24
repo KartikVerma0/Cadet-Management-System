9532569e72375c0dfc3c4291491a7ce5cdd52ab3
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-24 23:23:21 +0530

    Update: Added permission based rendering

--- frontend/src/components/specialNavbar/SpecialNavbar.jsx
+++ frontend/src/components/specialNavbar/SpecialNavbar.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import Model from '../modelComponent/Model'
 import './SpecialNavbar.css'
-
+import useAuth from '../../hooks/useAuth';
+import permissionsMapping from '../../permissionsMapping';
 
 
 export default function SpecialNavbar() {
     const [selectedButton, setSelectedButton] = useState("");
+    const { auth } = useAuth()
+    const permissions = auth.permissions
+    const canCreateEventCode = permissionsMapping.canCreateEvent
+    const canCreatePollCode = permissionsMapping.canCreatePoll
+    const canCreateNotificationCode = permissionsMapping.canCreateNotification
 
     const handleClick = (data) => {
         setSelectedButton(data);
@@ -13,13 +19,16 @@ export default function SpecialNavbar() {
 
     return (
         <>
-            <section className='SpecialNavbar'>
-                <button to="/create/event" onClick={() => handleClick("Event")}>Create an Event</button>
-                <button to="/create/poll" onClick={() => handleClick("Poll")}>Create a Poll</button>
-                <button to="/create/notification" onClick={() => handleClick("Notification")}>Create a Notification</button>
-            </section>
-            {selectedButton !== "" && <Model closeButtonHandler={setSelectedButton} topic={selectedButton} />}
-
+            {permissions.length ? permissions.includes(canCreateEventCode) || permissions.includes(canCreatePollCode) || permissions.includes(canCreateNotificationCode) ?
+                <>
+                    <section className='SpecialNavbar'>
+                        {permissions.length ? permissions.includes(canCreateEventCode) ? <button to="/create/event" onClick={() => handleClick("Event")}>Create an Event</button> : '' : ''}
+                        {permissions.length ? permissions.includes(canCreatePollCode) ? <button to="/create/poll" onClick={() => handleClick("Poll")}>Create a Poll</button> : '' : ''}
+                        {permissions.length ? permissions.includes(canCreateNotificationCode) ? <button to="/create/notification" onClick={() => handleClick("Notification")}>Create a Notification</button> : '' : ''}
+                    </section>
+                    {selectedButton !== "" && <Model closeButtonHandler={setSelectedButton} topic={selectedButton} />}
+                </>
+                : '' : ''}
         </>
 
     )
