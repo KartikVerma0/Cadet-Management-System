3bb33ab66217810137171818373e3caedfb03c3a
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-26 12:22:56 +0530

    Update: Added response functionality

--- frontend/src/components/box/Box.jsx
+++ frontend/src/components/box/Box.jsx
@@ -1,12 +1,87 @@
 import PropTypes from 'prop-types'
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
+import EastIcon from '@mui/icons-material/East';
+import MessageModal from '../messageModal/MessageModal';
+import { axiosPrivate } from '../../api/axios'
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom'
+
+//spinner component related imports
+import { spinner } from '../../hooks/useSpinner.jsx'
+import useSpinner from '../../hooks/useSpinner.jsx'
+
+import permissionsMapping from '../../permissionsMapping.js';
+
+import useAuth from '../../hooks/useAuth'
 import './Box.css'
 
 export default function Box({ info, section }) {
 
 
-    let { name, date, duration, description, startTime } = info;
+    let { name, date, duration, description, startTime, _id } = info;
+    const { auth } = useAuth()
+    const [hasResponded, setHasResponded] = useState(false)
+    const [responseMessage, setResponseMessage] = useState('')
+    const [hasError, setHasError] = useState(false)
+    const [selectedResponse, setSelectedResponse] = useState('')
+
+    const { spinnerVisible,
+        showSpinner,
+        hideSpinner
+    } = useSpinner()
+
+
+    useEffect(() => {
+        if (section !== 'notifications') {
+            const getResponse = async () => {
+                try {
+                    const res = await axiosPrivate.get(`/response/${section}?dataId=${_id}&email=${auth.email}`)
+                    setSelectedResponse(res.data.response)
+                } catch (err) {
+                    console.error(err)
+                }
+            }
+            getResponse()
+        }
+
+    }, [])
+
+
+
+
+    const handleResponse = async (response) => {
+        showSpinner()
+        setHasResponded(false)
+        const data = { dataId: _id, userEmail: auth.email, enrollmentNumber: auth.enrollmentNumber, nccWing: auth.nccWing, response };
+        let result = undefined;
+        try {
+            if (section === "polls") {
+                result = await axiosPrivate.post(`/response/${section}`, data)
+            } else if (section === "events") {
+                result = await axiosPrivate.post(`/response/${section}`, data)
+            }
+            if (result.data.success) {
+                setHasError(false)
+                setHasResponded(true)
+                setResponseMessage(result.data.message)
+                setSelectedResponse(response)
+            } else {
+                setHasError(true)
+                setHasResponded(true)
+                setResponseMessage(result.data.message)
+            }
+            hideSpinner()
+        } catch (err) {
+            setHasError(true)
+            setHasResponded(true)
+            setResponseMessage(err.message)
+            hideSpinner()
+            console.error(err.message)
+        }
+
+    }
+
     return (
         <div className='Box'>
             {name && <div>
@@ -32,12 +107,27 @@ export default function Box({ info, section }) {
             </div>
             }
 
-            {(section === "events" || section === "polls") &&
-                <div className='actions'>
-                    <button><CheckIcon /></button>
-                    <button><CloseIcon /></button>
-                </div>
+            {auth.permissions.includes(permissionsMapping.canRespondToEvent) && (section === "events") &&
+                <>
+                    <div className='actions'>
+                        <button onClick={() => { handleResponse(true) }} className={selectedResponse !== '' ? (selectedResponse === true ? '' : 'notSelected') : ''}><CheckIcon /></button>
+                        <button onClick={() => { handleResponse(false) }} className={selectedResponse !== '' ? (selectedResponse === false ? '' : 'notSelected') : ''}><CloseIcon /></button>
+                    </div>
+                    {spinnerVisible && spinner("rgba(0, 174, 239, 1)")}
+                </>
             }
+            {auth.permissions.includes(permissionsMapping.canRespondToPoll) && (section === "polls") &&
+                <>
+                    <div className='actions'>
+                        <button onClick={() => { handleResponse(true) }} className={selectedResponse !== '' ? (selectedResponse === true ? '' : 'notSelected') : ''}><CheckIcon /></button>
+                        <button onClick={() => { handleResponse(false) }} className={selectedResponse !== '' ? (selectedResponse === false ? '' : 'notSelected') : ''}><CloseIcon /></button>
+                    </div>
+                    {spinnerVisible && spinner("rgba(0, 174, 239, 1)")}
+                </>
+            }
+            {auth.permissions.includes(permissionsMapping.canSeeEventResponses) && (section === "events") && <Link to={`/responses/events/${_id}`} className='responses'><span>Responses</span><EastIcon /></Link>}
+            {auth.permissions.includes(permissionsMapping.canSeePollResponses) && (section === "polls") && <Link to={`/responses/polls/${_id}`} className='responses'><span>Responses</span><EastIcon /></Link>}
+            {hasResponded && <MessageModal closeButtonHandler={setHasResponded} message={responseMessage} hasError={hasError} />}
         </div>
     )
 }
