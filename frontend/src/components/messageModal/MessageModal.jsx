887cbc59d346a6e31c5428afb7de1e8087af27ad
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-26 12:23:16 +0530

    Update: Added error response animation

--- frontend/src/components/messageModal/MessageModal.jsx
+++ frontend/src/components/messageModal/MessageModal.jsx
@@ -2,18 +2,27 @@ import PropTypes from 'prop-types'
 import CloseIcon from '@mui/icons-material/Close';
 import Lottie from 'react-lottie'
 import ResponseLottieAnimation from '../../assets/response_animation.json'
+import ErrorLottieAnimation from '../../assets/error_animation.json'
 
 import "./MessageModal.css"
 import { useEffect } from 'react';
 
 
 
-export default function MessageModal({ closeButtonHandler, message }) {
+export default function MessageModal({ closeButtonHandler, message, hasError }) {
 
-    const lottieOptions = {
+    const successLottieOptions = {
         loop: true,
         autoplay: true,
         animationData: ResponseLottieAnimation,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    }
+    const errorLottieOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: ErrorLottieAnimation,
         rendererSettings: {
             preserveAspectRatio: "xMidYMid slice"
         }
@@ -28,10 +37,17 @@ export default function MessageModal({ closeButtonHandler, message }) {
     return (
         <div className="MessageModal">
             <main>
-                <Lottie options={lottieOptions}
-                    height={400}
-                    width={400}
-                />
+                {!hasError ?
+                    <Lottie options={successLottieOptions}
+                        height={400}
+                        width={400}
+                    />
+                    :
+                    <Lottie options={errorLottieOptions}
+                        height={400}
+                        width={400}
+                    />
+                }
                 {message}
             </main>
             <button onClick={() => { closeButtonHandler(false) }}><CloseIcon /></button>
@@ -41,5 +57,6 @@ export default function MessageModal({ closeButtonHandler, message }) {
 
 MessageModal.propTypes = {
     closeButtonHandler: PropTypes.func.isRequired,
-    message: PropTypes.string.isRequired
+    message: PropTypes.string.isRequired,
+    hasError: PropTypes.bool.isRequired
 }
