2594e966cf16daca099b26d4c5131bed24f74698
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-26 12:17:04 +0530

    Update: Added new permissions

--- frontend/src/permissionsMapping.js
+++ frontend/src/permissionsMapping.js
@@ -1,6 +1,10 @@
 const permissionsMapping = {
     canCreateEvent: 335,
     canCreatePoll: 3316,
-    canCreateNotification: 3314
+    canCreateNotification: 3314,
+    canRespondToEvent: 318205,
+    canRespondToPoll: 3182016,
+    canSeeEventResponses: 319518,
+    canSeePollResponses: 3191618,
 }
 export default permissionsMapping
