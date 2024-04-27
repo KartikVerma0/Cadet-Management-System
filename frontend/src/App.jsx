2257884a851d87f0ac20e2d5993a0c62ac894ec8
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-28 02:20:44 +0530

    Update: Added permission based response list viewing functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,22 @@ import SignUpRouterPage from "./pages/signUpRouterPage/SignUpRouterPage"
 import LoginPage from "./pages/loginPage/LoginPage"
 import SignUpPage from "./pages/signupPage/SignUpPage"
 import Dashboard from "./pages/dashboard/Dashboard"
-import ResponsesList from "./components/responsesList/ResponsesList"
+import ResponsesListPoll from "./pages/responsesListPoll/ResponsesListPoll"
+import ResponsesListEvent from "./pages/responseListEvent/ResponseListEvent"
 import FourOFourPage from "./pages/404Page/FourOFourPage"
 
 import { Analytics } from "@vercel/analytics/react"
 
 import { Route, Routes } from "react-router-dom"
 
+import permissionsMapping from "./permissionsMapping"
+import useAuth from "./hooks/useAuth"
+
 import "./App.css"
 
 function App() {
+  const { auth } = useAuth()
+
 
   return (
     <>
@@ -24,7 +30,8 @@ function App() {
         <Route path="/signup/:role" element={<SignUpPage />} />
         {/* if logged in then show dashboard */}
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/responses/:section/:dataId" element={<ResponsesList />} />
+        {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeePollResponses) && <Route path="/responses/polls/:dataId" element={<ResponsesListPoll />} />}
+        {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeeEventResponses) && <Route path="/responses/events/:dataId" element={<ResponsesListEvent />} />}
         <Route path="*" element={<FourOFourPage />} />
       </Routes>
     </>
