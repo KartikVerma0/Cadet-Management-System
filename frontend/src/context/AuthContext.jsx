d9fd80fb4befff58f362bb101dd78753e007da89
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-28 02:24:21 +0530

    Removed comment

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -6,7 +6,6 @@ export default AuthContext
 
 export function AuthProvider({ children }) {
     const [auth, setAuth] = useState({})
-    // console.log(auth)
 
     return (
         <AuthContext.Provider value={{ auth, setAuth }}>
