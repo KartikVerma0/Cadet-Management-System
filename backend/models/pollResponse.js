be22fb0da3c5866b45271f2310d96e39f1e2395e
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-28 02:13:51 +0530

    Update: Added new fields and changed schema of old fields

--- backend/models/pollResponse.js
+++ backend/models/pollResponse.js
@@ -8,18 +8,43 @@ const pollResponseSchema = new Schema({
         ref: 'Poll',
         required: true
     },
-    userDetails: {
-        email: {
-            type: String,
-            required: true,
-        },
-        enrollmentNumber: {
-            type: String,
-        },
-        nccWing: {
-            type: String,
-            required: true
-        },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    enrollmentNumber: {
+        type: String,
+    },
+    nccWing: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String
+    },
+    mobileNumber: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    rollNumber: {
+        type: Number,
+        required: true,
+    },
+    academicYear: {
+        type: Number,
+        required: true
     },
     response: {
         type: Boolean,
