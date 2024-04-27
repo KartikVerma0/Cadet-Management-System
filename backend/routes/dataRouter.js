62dedc063fe032f388cf59d10cf95a2014a59ac8
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-28 02:18:11 +0530

    Update: Added polldata and eventdata routes which gives info about individual poll or event

--- backend/routes/dataRouter.js
+++ backend/routes/dataRouter.js
@@ -5,12 +5,44 @@ import Notification from '../models/notification.js';
 
 const Router = express.Router();
 
+Router.get("/eventdata", async (req, res) => {
+    const { eventId } = req.query;
+    let eventInfo = undefined;
+    try {
+        eventInfo = await Event.findById(eventId)
+    } catch (err) {
+        return res.json({ success: false, message: "Server Error" }).status(500)
+    }
+
+    if (eventInfo) {
+        return res.json({ success: true, eventInfo })
+    }
+
+    return res.json({ success: false, message: "Invalid event selected" }).status(404)
+})
+
 Router.get("/event", async (req, res) => {
     const events = await Event.find()
     return res.json({
         success: true,
         data: events
     })
+})
+
+Router.get("/polldata", async (req, res) => {
+    const { pollId } = req.query;
+    let pollInfo = undefined;
+    try {
+        pollInfo = await Poll.findById(pollId)
+    } catch (err) {
+        return res.json({ success: false, message: "Server Error" }).status(500)
+    }
+
+    if (pollInfo) {
+        return res.json({ success: true, pollInfo })
+    }
+
+    return res.json({ success: false, message: "Invalid poll selected" }).status(404)
 })
 
 Router.get("/poll", async (req, res) => {
