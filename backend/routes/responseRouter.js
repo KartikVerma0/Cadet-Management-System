811722a9b80ea95bea9c4dac3c349e1d32d2968e
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-28 02:19:50 +0530

    Update: Added event response related routes and  poll all responses route

--- backend/routes/responseRouter.js
+++ backend/routes/responseRouter.js
@@ -1,16 +1,33 @@
 import express from 'express'
 import Poll from '../models/poll.js'
+import Event from '../models/event.js'
 import PollResponse from '../models/pollResponse.js';
+import EventResponse from '../models/eventResponse.js'
 import permissionsMapping from '../config/permissionsMapping.js'
 import verifyPermissions from '../middleware/verifyPermissions.js'
 
 const Router = express.Router()
 
+Router.get("/allpollresponses", verifyPermissions(permissionsMapping.canSeePollResponses), async (req, res) => {
+    const { pollId } = req.query;
+    let pollResponses = undefined;
+    try {
+        pollResponses = await PollResponse.find({ pollId })
+    } catch (err) {
+        return res.json({ success: false, message: "Server Error" }).status(500)
+    }
+
+    if (pollResponses.length) {
+        return res.json({ success: true, pollResponses })
+    }
+    return res.json({ success: false, message: "No responses found" })
+
+})
 Router.get("/polls", async (req, res) => {
     const { dataId: pollId, email } = req.query;
     let response = undefined;
     try {
-        response = await PollResponse.findOne({ pollId, 'userDetails.email': email })
+        response = await PollResponse.findOne({ pollId, email })
     } catch (err) {
         return res.json({ success: false, message: "Server Error" }).status(500)
     }
@@ -24,7 +41,7 @@ Router.get("/polls", async (req, res) => {
 })
 
 Router.post("/polls", verifyPermissions(permissionsMapping.canRespondToPoll), async (req, res) => {
-    const { dataId, userEmail, enrollmentNumber, nccWing, response } = req.body;
+    const { dataId, userName, userEmail, enrollmentNumber, nccWing, address, mobileNumber, gender, department, rollNumber, academicYear, response } = req.body;
     let selectedPoll = undefined;
     try {
         selectedPoll = await Poll.findById(dataId)
@@ -34,7 +51,7 @@ Router.post("/polls", verifyPermissions(permissionsMapping.canRespondToPoll), as
     // console.log(selectedPoll)
     try {
 
-        const selectedPollResponse = await PollResponse.findOne({ pollId: dataId, 'userDetails.email': userEmail })
+        const selectedPollResponse = await PollResponse.findOne({ pollId: dataId, email: userEmail })
 
         if (selectedPollResponse) {
             selectedPollResponse.response = response
@@ -42,12 +59,14 @@ Router.post("/polls", verifyPermissions(permissionsMapping.canRespondToPoll), as
         } else {
             const newResponse = new PollResponse({
                 pollId: dataId,
-                userDetails: {
-                    email: userEmail,
-                    enrollmentNumber: enrollmentNumber,
-                    nccWing: nccWing,
-                },
-                response: response
+                name: userName,
+                email: userEmail,
+                enrollmentNumber,
+                nccWing,
+                address, mobileNumber,
+                gender, department,
+                rollNumber, academicYear,
+                response
             })
             await newResponse.save()
         }
@@ -59,14 +78,73 @@ Router.post("/polls", verifyPermissions(permissionsMapping.canRespondToPoll), as
     return res.json({ success: true, message: "Successfully recorded your response" })
 })
 
+
+Router.get("/alleventresponses", async (req, res) => {
+    const { eventId } = req.query;
+    let eventResponses = undefined;
+    try {
+        eventResponses = await EventResponse.find({ eventId })
+    } catch (err) {
+        return res.json({ success: false, message: "Server Error" }).status(500)
+    }
+
+    if (eventResponses.length) {
+        return res.json({ success: true, eventResponses })
+    }
+    return res.json({ success: false, message: "No responses found" })
+})
+
 Router.get("/events", async (req, res) => {
     const { dataId: eventId, email } = req.query;
+    let response = undefined;
+    try {
+        response = await EventResponse.findOne({ eventId, email })
+    } catch (err) {
+        return res.json({ success: false, message: "Server Error" }).status(500)
+    }
+
+    if (response) {
+        return res.json({ success: true, response: response.response })
+    }
 
     return res.json({ success: false, message: "No previous response found" })
 
 })
 
 Router.post("/events", verifyPermissions(permissionsMapping.canRespondToEvent), async (req, res) => {
-    res.json({ success: true, message: "Successfully recorded your response" })
+    const { dataId, userName, userEmail, enrollmentNumber, nccWing, address, mobileNumber, gender, department, rollNumber, academicYear, response } = req.body;
+    let selectedEvent = undefined;
+    try {
+        selectedEvent = await Event.findById(dataId)
+    } catch (err) {
+        return res.json({ success: false, message: "Invalid Event Selected" })
+    }
+    try {
+
+        const selectedEventResponse = await EventResponse.findOne({ eventId: dataId, email: userEmail })
+
+        if (selectedEventResponse) {
+            selectedEventResponse.response = response
+            await selectedEventResponse.save()
+        } else {
+            const newResponse = new EventResponse({
+                eventId: dataId,
+                name: userName,
+                email: userEmail,
+                enrollmentNumber,
+                nccWing,
+                address, mobileNumber,
+                gender, department,
+                rollNumber, academicYear,
+                response
+            })
+            await newResponse.save()
+        }
+    } catch (err) {
+        console.log(err)
+        return res.json({ success: false, message: "Problem recording your response, Error: \n" + err })
+    }
+
+    return res.json({ success: true, message: "Successfully recorded your response" })
 })
 export default Router
