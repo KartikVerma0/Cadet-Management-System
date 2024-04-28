0738f5ff1f50fc62c143f00d69770d3f3ab8c3b0
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-28 11:38:56 +0530

    Update: Shifted response handling logic to their respective controller functions

--- backend/routes/responseRouter.js
+++ backend/routes/responseRouter.js
@@ -1,82 +1,29 @@
 import express from 'express'
-import Poll from '../models/poll.js'
-import Event from '../models/event.js'
-import PollResponse from '../models/pollResponse.js';
-import EventResponse from '../models/eventResponse.js'
+
 import permissionsMapping from '../config/permissionsMapping.js'
 import verifyPermissions from '../middleware/verifyPermissions.js'
+
+import getAllResponsesOfAPoll from '../controllers/getAllResponsesOfAPoll.js';
+import getAllResponsesOfAnEvent from '../controllers/getAllResponsesOfAnEvent.js';
+
+import getPollResponseOfAPerson from '../controllers/getPollResponseOfAPerson.js';
+import getEventResponseOfAPerson from '../controllers/getEventResponseOfAPerson.js';
 
 import respondToEvent from '../controllers/respondToEvent.js';
 import respondToPoll from '../controllers/respondToPoll.js';
 
 const Router = express.Router()
 
-Router.get("/allpollresponses", verifyPermissions(permissionsMapping.canSeePollResponses), async (req, res) => {
-    const { pollId } = req.query;
-    let pollResponses = undefined;
-    try {
-        pollResponses = await PollResponse.find({ pollId })
-    } catch (err) {
-        return res.json({ success: false, message: "Server Error" }).status(500)
-    }
+Router.get("/allpollresponses", verifyPermissions(permissionsMapping.canSeePollResponses), getAllResponsesOfAPoll)
 
-    if (pollResponses.length) {
-        return res.json({ success: true, pollResponses })
-    }
-    return res.json({ success: false, message: "No responses found" })
-
-})
-Router.get("/polls", async (req, res) => {
-    const { dataId: pollId, email } = req.query;
-    let response = undefined;
-    try {
-        response = await PollResponse.findOne({ pollId, email })
-    } catch (err) {
-        return res.json({ success: false, message: "Server Error" }).status(500)
-    }
-
-    if (response) {
-        return res.json({ success: true, response: response.response })
-    }
-
-    return res.json({ success: false, message: "No previous response found" })
-
-})
+Router.get("/polls", getPollResponseOfAPerson)
 
 Router.post("/polls", verifyPermissions(permissionsMapping.canRespondToPoll), respondToPoll)
 
 
-Router.get("/alleventresponses", async (req, res) => {
-    const { eventId } = req.query;
-    let eventResponses = undefined;
-    try {
-        eventResponses = await EventResponse.find({ eventId })
-    } catch (err) {
-        return res.json({ success: false, message: "Server Error" }).status(500)
-    }
+Router.get("/alleventresponses", verifyPermissions(permissionsMapping.canSeeEventResponses), getAllResponsesOfAnEvent)
 
-    if (eventResponses.length) {
-        return res.json({ success: true, eventResponses })
-    }
-    return res.json({ success: false, message: "No responses found" })
-})
-
-Router.get("/events", async (req, res) => {
-    const { dataId: eventId, email } = req.query;
-    let response = undefined;
-    try {
-        response = await EventResponse.findOne({ eventId, email })
-    } catch (err) {
-        return res.json({ success: false, message: "Server Error" }).status(500)
-    }
-
-    if (response) {
-        return res.json({ success: true, response: response.response })
-    }
-
-    return res.json({ success: false, message: "No previous response found" })
-
-})
+Router.get("/events", getEventResponseOfAPerson)
 
 Router.post("/events", verifyPermissions(permissionsMapping.canRespondToEvent), respondToEvent)
 export default Router
