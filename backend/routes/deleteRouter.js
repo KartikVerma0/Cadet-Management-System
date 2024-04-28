b01502fe3dea6e9cfece8433c6601a508a6d2e05
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-28 14:32:01 +0530

    Initialized delete router

---
+++ backend/routes/deleteRouter.js
@@ -0,0 +1,58 @@
+import express from 'express'
+
+import Event from '../models/event.js'
+import EventResponse from '../models/eventResponse.js'
+import Poll from '../models/poll.js'
+import PollResponse from '../models/pollResponse.js'
+import Notification from '../models/notification.js'
+
+const Router = express.Router();
+
+Router.delete('/events', async (req, res) => {
+    const { eventsId } = req.query
+
+    try {
+        await Event.deleteOne({ _id: eventsId })
+    } catch (err) {
+        return res.json({ success: false, message: "Problem deleting Event" }).status(500)
+    }
+
+    try {
+        await EventResponse.deleteMany({ eventId: eventsId })
+    } catch (err) {
+        return res.json({ success: false, message: "Deleted Event but Problem deleting Event Responses" }).status(500)
+    }
+
+    return res.json({ success: true, message: "Successfully deleted Event" })
+})
+
+Router.delete('/polls', async (req, res) => {
+    const { pollsId } = req.query
+
+    try {
+        await Poll.deleteOne({ _id: pollsId })
+    } catch (err) {
+        return res.json({ success: false, message: "Problem deleting Poll" }).status(500)
+    }
+
+    try {
+        await PollResponse.deleteMany({ pollId: pollsId })
+    } catch (err) {
+        return res.json({ success: false, message: "Deleted Poll but Problem deleting Poll Responses" }).status(500)
+    }
+
+    return res.json({ success: true, message: "Successfully deleted Poll" })
+})
+
+Router.delete('/notifications', async (req, res) => {
+    const { notificationsId } = req.query
+
+    try {
+        await Notification.deleteOne({ _id: notificationsId })
+        return res.json({ success: true, message: "Successfully deleted Notification" })
+    } catch (err) {
+        return res.json({ success: false, message: "Problem deleting Notification" }).status(500)
+    }
+})
+
+export default Router
