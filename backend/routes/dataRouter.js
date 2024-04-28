c29b9d35c27fe63e0b80470fa037cb92f0dd3d9c
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-04-28 11:46:49 +0530

    Update: Shifted getting poll data logic to getPollData controller file

--- backend/routes/dataRouter.js
+++ backend/routes/dataRouter.js
@@ -4,6 +4,7 @@ import Poll from '../models/poll.js';
 import Notification from '../models/notification.js';
 
 import getEventData from '../controllers/getEventData.js';
+import getPollData from '../controllers/getPollData.js';
 
 const Router = express.Router();
 
@@ -17,21 +18,7 @@ Router.get("/event", async (req, res) => {
     })
 })
 
-Router.get("/polldata", async (req, res) => {
-    const { pollId } = req.query;
-    let pollInfo = undefined;
-    try {
-        pollInfo = await Poll.findById(pollId)
-    } catch (err) {
-        return res.json({ success: false, message: "Server Error" }).status(500)
-    }
-
-    if (pollInfo) {
-        return res.json({ success: true, pollInfo })
-    }
-
-    return res.json({ success: false, message: "Invalid poll selected" }).status(404)
-})
+Router.get("/polldata", getPollData)
 
 Router.get("/poll", async (req, res) => {
     const polls = await Poll.find()
