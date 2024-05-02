8492d98f2c37c260e3639f9bd7d05fa18d9de683
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-02 22:56:39 +0530

    update: add check route which checks if an email being used is unique or not

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 import signupController from '../controllers/signupController.js'
 import loginController from '../controllers/loginController.js'
-
+import checkController from '../controllers/checkController.js'
 
 import { handleLogout } from '../utils/authToken.js'
 const router = express.Router();
@@ -10,6 +10,8 @@ const router = express.Router();
 router.post("/signup/:role", signupController)
 
 router.post('/login/:role', loginController)
+
+router.get('/check', checkController)
 
 
 router.get("/logout/:role", handleLogout)
