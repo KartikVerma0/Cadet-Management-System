06b988c77f0964ae54fff847e219665f65b41927
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-02 22:59:56 +0530

    update: add /account and /studymaterial routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,9 @@ import SignUpPage from "./pages/signupPage/SignUpPage"
 import Dashboard from "./pages/dashboard/Dashboard"
 import ResponsesListPoll from "./pages/responsesListPoll/ResponsesListPoll"
 import ResponsesListEvent from "./pages/responseListEvent/ResponseListEvent"
+import AccountPage from "./pages/accountPage/AccountPage"
 import FourOFourPage from "./pages/404Page/FourOFourPage"
+import StudyMaterialPage from "./pages/studyMaterialPage/StudyMaterialPage"
 
 import { Analytics } from "@vercel/analytics/react"
 
@@ -32,6 +34,8 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeePollResponses) && <Route path="/responses/polls/:dataId" element={<ResponsesListPoll />} />}
         {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeeEventResponses) && <Route path="/responses/events/:dataId" element={<ResponsesListEvent />} />}
+        {auth.accessToken && <Route path="/account" element={<AccountPage />} />}
+        {auth.accessToken && <Route path="/studymaterial" element={<StudyMaterialPage />} />}
         <Route path="*" element={<FourOFourPage />} />
       </Routes>
     </>
