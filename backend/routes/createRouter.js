d8cac3ab0d863fb5cd8d0105e60a7ece1e391e81
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-02 22:56:09 +0530

    update: add create study material, excuse routes

--- backend/routes/createRouter.js
+++ backend/routes/createRouter.js
@@ -6,20 +6,25 @@ import verifyPermissions from '../middleware/verifyPermissions.js';
 import Event from '../models/event.js';
 import Poll from '../models/poll.js';
 import Notification from '../models/notification.js';
+import StudyMaterial from '../models/studyMaterial.js';
+import Excuse from '../models/excuse.js';
+import EventResponse from '../models/eventResponse.js'
 
 import uploadImages from '../middleware/uploadImages.js';
+import uploadFiles from '../middleware/uploadFiles.js';
 
 const Router = express.Router();
 
 Router.post("/event", verifyPermissions(permissionsMapping.canCreateEvent), uploadImages({ folder: 'event' }), async (req, res) => {
-    const { Event_name, Event_date, start_time, Event_duration, Event_description } = req.body;
+    const { Event_name, Event_date, start_time, Event_duration, Event_description, createdBy } = req.body;
     const newEvent = new Event({
         name: Event_name,
         date: Event_date,
         startTime: start_time,
         duration: Event_duration,
         description: Event_description,
-        images: req.files
+        images: req.files,
+        createdBy
     })
     try {
         await newEvent.save()
@@ -30,11 +35,12 @@ Router.post("/event", verifyPermissions(permissionsMapping.canCreateEvent), uplo
 })
 
 Router.post("/poll", verifyPermissions(permissionsMapping.canCreatePoll), uploadImages({ folder: 'poll' }), async (req, res) => {
-    const { Poll_name, Poll_description } = req.body;
+    const { Poll_name, Poll_description, createdBy } = req.body;
     const newPoll = new Poll({
         name: Poll_name,
         description: Poll_description,
-        images: req.files
+        images: req.files,
+        createdBy
     })
     try {
         await newPoll.save()
@@ -45,11 +51,12 @@ Router.post("/poll", verifyPermissions(permissionsMapping.canCreatePoll), upload
 })
 
 Router.post("/notification", verifyPermissions(permissionsMapping.canCreateNotification), uploadImages({ folder: 'notification' }), async (req, res) => {
-    const { Notification_name, Notification_description } = req.body;
+    const { Notification_name, Notification_description, createdBy } = req.body;
     const newNotification = new Notification({
         name: Notification_name,
         description: Notification_description,
-        images: req.files
+        images: req.files,
+        createdBy
     })
     try {
         await newNotification.save()
@@ -58,6 +65,51 @@ Router.post("/notification", verifyPermissions(permissionsMapping.canCreateNotif
         return res.json({ success: false, message: "Problem saving notification data" })
     }
 
+})
+
+Router.post("/study_material", verifyPermissions(permissionsMapping.canUploadStudyMaterial), uploadFiles(), async (req, res) => {
+    const { Study_Material_name, Study_Material_description, nccWing, createdBy } = req.body;
+    const newStudyMaterial = new StudyMaterial({
+        name: Study_Material_name,
+        description: Study_Material_description,
+        nccWing,
+        createdBy,
+        files: req.files
+    })
+    try {
+        await newStudyMaterial.save()
+        return res.json({ success: true, message: "Successfully got Study Material data" })
+    } catch (err) {
+        return res.json({ success: false, message: "Problem saving Study Material data, Error:\n" + err.message })
+    }
+})
+
+Router.post("/excuse", verifyPermissions(permissionsMapping.canRespondToEvent), uploadFiles(), async (req, res) => {
+    const { Excuse_description, reasonCategory, createdBy, eventId, userEmail, role, nccWing } = req.body;
+
+    try {
+        await Excuse.findOneAndDelete({ eventId, userEmail })
+    } catch (err) {
+        return res.json({ success: false, message: "Problem deleting old excuse data, Error:\n" + err.message }).status(500)
+    }
+
+    const newExcuse = new Excuse({
+        description: Excuse_description,
+        reasonCategory,
+        createdBy,
+        userEmail,
+        nccWing,
+        role,
+        files: req.files,
+        eventId,
+    })
+    try {
+        await newExcuse.save()
+        await EventResponse.findOneAndDelete({ eventId, email: userEmail })
+        return res.json({ success: true, message: "Successfully saved excuse data" })
+    } catch (err) {
+        return res.json({ success: false, message: "Problem saving excuse data, Error:\n" + err.message }).status(500)
+    }
 })
 
 export default Router
