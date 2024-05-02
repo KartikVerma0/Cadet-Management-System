29c9899b1e8df02abec444db175453e16d3f4381
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-02 23:10:16 +0530

    update: add upload study material button, seeing enrolled cadets and probation cadets links

--- frontend/src/components/specialNavbar/SpecialNavbar.jsx
+++ frontend/src/components/specialNavbar/SpecialNavbar.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Model from '../modelComponent/Model'
 import './SpecialNavbar.css'
 import useAuth from '../../hooks/useAuth';
+import { Link } from 'react-router-dom'
 import permissionsMapping from '../../permissionsMapping';
 
 
@@ -9,9 +10,6 @@ export default function SpecialNavbar() {
     const [selectedButton, setSelectedButton] = useState("");
     const { auth } = useAuth()
     const permissions = auth.permissions
-    const canCreateEventCode = permissionsMapping.canCreateEvent
-    const canCreatePollCode = permissionsMapping.canCreatePoll
-    const canCreateNotificationCode = permissionsMapping.canCreateNotification
 
     const handleClick = (data) => {
         setSelectedButton(data);
@@ -19,16 +17,26 @@ export default function SpecialNavbar() {
 
     return (
         <>
-            {permissions.length ? permissions.includes(canCreateEventCode) || permissions.includes(canCreatePollCode) || permissions.includes(canCreateNotificationCode) ?
-                <>
-                    <section className='SpecialNavbar'>
-                        {permissions.length ? permissions.includes(canCreateEventCode) ? <button to="/create/event" onClick={() => handleClick("Event")}>Create an Event</button> : '' : ''}
-                        {permissions.length ? permissions.includes(canCreatePollCode) ? <button to="/create/poll" onClick={() => handleClick("Poll")}>Create a Poll</button> : '' : ''}
-                        {permissions.length ? permissions.includes(canCreateNotificationCode) ? <button to="/create/notification" onClick={() => handleClick("Notification")}>Create a Notification</button> : '' : ''}
-                    </section>
-                    {selectedButton !== "" && <Model closeButtonHandler={setSelectedButton} topic={selectedButton} />}
-                </>
-                : '' : ''}
+            {permissions.length ?
+                permissions.includes(permissionsMapping.canCreateEvent) ||
+                    permissions.includes(permissionsMapping.canCreatePollCode) ||
+                    permissions.includes(permissionsMapping.canCreateNotificationCode) ||
+                    permissions.includes(permissionsMapping.canSeeEnrolledCadets) ||
+                    permissions.includes(permissionsMapping.canSeeProbationCadets) ||
+                    permissions.includes(permissionsMapping.canUploadStudyMaterial)
+                    ?
+                    <>
+                        <section className='SpecialNavbar'>
+                            {permissions.includes(permissionsMapping.canCreateEvent) ? <button onClick={() => handleClick("Event")}>Create an Event</button> : ''}
+                            {permissions.includes(permissionsMapping.canCreatePoll) ? <button onClick={() => handleClick("Poll")}>Create a Poll</button> : ''}
+                            {permissions.includes(permissionsMapping.canCreateNotification) ? <button onClick={() => handleClick("Notification")}>Create a Notification</button> : ''}
+                            {permissions.includes(permissionsMapping.canUploadStudyMaterial) ? <button onClick={() => handleClick("Study_Material")}>Upload Study Material</button> : ''}
+                            {permissions.includes(permissionsMapping.canSeeEnrolledCadets) ? <Link to="/enrolled/cadets">Enrolled Cadets</Link> : ''}
+                            {permissions.includes(permissionsMapping.canSeeProbationCadets) ? <Link to="/enrolled/probation">Probation Cadets</Link> : ''}
+                        </section>
+                        {selectedButton !== "" && <Model closeButtonHandler={setSelectedButton} topic={selectedButton} />}
+                    </>
+                    : '' : ''}
         </>
 
     )
