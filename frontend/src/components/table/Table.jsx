932a3acc89602790f811f250c9329079496c8728
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-04 13:37:42 +0530

    fix: Added more columns (gender, department, academic year, roll number, mobile number) and their styling

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -14,10 +14,15 @@ const Table = ({ tableData }) => {
                 <tr className='headingRow'>
                     <th>S. No.</th>
                     {columns?.includes('name') && <th>Name</th>}
+                    {columns?.includes('gender') && <th>Gender</th>}
                     {columns?.includes('nccWing') && <th>Wing</th>}
                     {columns?.includes('nccUnit') && <th>Unit</th>}
                     {columns?.includes('enrollmentNumber') && <th>Enrollment No.</th>}
+                    {columns?.includes('department') && <th>Department</th>}
+                    {columns?.includes('academicYear') && <th>Academic Year</th>}
+                    {columns?.includes('rollNumber') && <th>Roll Number</th>}
                     {columns?.includes('email') && <th>Email</th>}
+                    {columns?.includes('mobileNumber') && <th>Mobile Number</th>}
                     {columns?.includes('response') && <th>Response</th>}
                 </tr>
             </thead>
@@ -27,10 +32,15 @@ const Table = ({ tableData }) => {
                         <tr key={index}>
                             <td>{index + 1}</td>
                             {data.name && <td>{data.name}</td>}
+                            {data.gender && <td>{data.gender}</td>}
                             {data.nccWing && <td>{data.nccWing}</td>}
                             {data.nccUnit && <td>{data.nccUnit.toUpperCase()}</td>}
                             {data.enrollmentNumber && <td>{data.enrollmentNumber.toUpperCase()}</td>}
+                            {data.department && <td>{data.department.toUpperCase()}</td>}
+                            {data.academicYear && <td>{data.academicYear}</td>}
+                            {data.rollNumber && <td>{data.rollNumber}</td>}
                             {data.email && <td>{data.email}</td>}
+                            {data.mobileNumber && <td>{data.mobileNumber}</td>}
                             {data.response !== undefined && <td className={data.response ? 'font-green' : 'font-red'}>{data.response ? 'YES' : 'NO'}</td>}
                         </tr>
                     )
