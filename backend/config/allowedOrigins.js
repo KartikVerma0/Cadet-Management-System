a3122e3d830fe1712931ed7ca25234836c2738c1
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-08 12:33:08 +0530

    update: added cloudflare pages frontend url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,4 +1,5 @@
 export const allowedOrigins = [
     'https://chitkara-ncc-cms.vercel.app',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://cadet-management-system.pages.dev'
 ]
