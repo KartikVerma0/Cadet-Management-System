8541c2391d546f14e96aafaef230748239975f71
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-08 18:15:40 +0530

    update: added get unapproved cadet, approve and delete routes

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,6 +1,9 @@
+import approveCadet from "../controllers/approveCadet.js";
 import changeUserPassword from "../controllers/changeUserPassword.js";
+import deleteCadet from "../controllers/deleteCadet.js";
 import demoteCadet from "../controllers/demoteCadet.js";
 import express from "express";
+import getUnapprovedCadet from "../controllers/getUnapprovedCadets.js";
 import getUserData from "../controllers/getUserData.js";
 import permissionsMapping from "../config/permissionsMapping.js";
 import promoteCadet from "../controllers/promoteCadet.js";
@@ -14,5 +17,8 @@ Router.post("/", setUserData)
 Router.post("/changePwd", changeUserPassword)
 Router.put("/promote", verifyPermissions(permissionsMapping.canAuthorizeWingSenior, permissionsMapping.canAuthorizeProbationSenior), promoteCadet)
 Router.put("/demote", verifyPermissions(permissionsMapping.canDemoteWingSenior, permissionsMapping.canDemoteProbationSenior), demoteCadet)
+Router.get("/unapproved", verifyPermissions(permissionsMapping.canApproveCadetAccounts), getUnapprovedCadet)
+Router.put("/approve", verifyPermissions(permissionsMapping.canApproveCadetAccounts), approveCadet)
+Router.delete("/", verifyPermissions(permissionsMapping.canApproveCadetAccounts), deleteCadet)
 
 export default Router
