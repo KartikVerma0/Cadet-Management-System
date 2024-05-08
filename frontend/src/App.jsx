6dbcd46d9935643b4a705190c668d7ff42e3f2c8
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-08 18:19:11 +0530

    update: added approve account page route and unapproved accounts will be redirected to not approved page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import AccountPage from "./pages/accountPage/AccountPage";
+import ApproveCadetAccountPage from "./pages/approveCadetAccountPage/ApproveCadetAccountPage";
 import AuthorizeCadetsPage from "./pages/authorizeCadetsPage/AuthorizeCadetsPage";
 import CampDetailsPage from "./pages/campDetailsPage/CampDetailsPage";
 import Dashboard from "./pages/dashboard/Dashboard";
@@ -9,6 +10,7 @@ import FourOFourPage from "./pages/404Page/FourOFourPage";
 import IndexPage from "./pages/indexPage/IndexPage";
 import LoginPage from "./pages/loginPage/LoginPage";
 import NotificationsPage from "./pages/notificationsPage/NotificationsPage";
+import PendingAccountApprovalPage from "./pages/pendingAccountApprovalPage/PendingAccountApprovalPage";
 import PendingExcusesPage from "./pages/pendingExcusesPage/PendingExcusesPage";
 import PollsPage from "./pages/pollsPage/PollsPage";
 import ProbationCadets from "./pages/probationCadetsPage/ProbationCadets";
@@ -21,7 +23,7 @@ import StudyMaterialPage from "./pages/studyMaterialPage/StudyMaterialPage";
 import permissionsMapping from "./permissionsMapping";
 import useAuth from "./hooks/useAuth";
 import { Analytics } from "@vercel/analytics/react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 
 function App() {
   const { auth } = useAuth()
@@ -35,24 +37,28 @@ function App() {
         <Route path="/login/:role" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpRouterPage />} />
         <Route path="/signup/:role" element={<SignUpPage />} />
+        <Route path="/notApproved" element={<PendingAccountApprovalPage />} />
         {/* if logged in then show dashboard */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeePollResponses) && <Route path="/responses/polls/:dataId" element={<ResponsesListPoll />} />}
-        {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeeEventResponses) && <Route path="/responses/events/:dataId" element={<ResponsesListEvent />} />}
-        {auth.accessToken && auth.permissions.includes(permissionsMapping.canApprovePendingExcuses) && <Route path="/pendingexcuses" element={<PendingExcusesPage />} />}
-        {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeeEnrolledCadets) && <Route path="/enrolled/cadets" element={<EnrolledCadets />} />}
-        {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeeProbationCadets) && <Route path="/enrolled/probation" element={<ProbationCadets />} />}
-        {auth.accessToken && (
-          auth.permissions.includes(permissionsMapping.canAuthorizeWingSenior) ||
-          auth.permissions.includes(permissionsMapping.canAuthorizeProbationSenior)
-        ) && <Route path="/authorize" element={<AuthorizeCadetsPage />} />}
-        {auth.accessToken && <Route path="/events" element={<EventsPage />} />}
-        {auth.accessToken && <Route path="/polls" element={<PollsPage />} />}
-        {auth.accessToken && <Route path="/notifications" element={<NotificationsPage />} />}
-        {auth.accessToken && <Route path="/account" element={<AccountPage />} />}
-        {auth.accessToken && <Route path="/settings" element={<SettingsPage />} />}
-        {auth.accessToken && <Route path="/studymaterial" element={<StudyMaterialPage />} />}
-        {auth.accessToken && <Route path="/campdetails" element={<CampDetailsPage />} />}
+        <Route element={auth.accountApproved ? <Outlet /> : <Navigate to={'/notApproved'} replace />} >
+          <Route path="/dashboard" element={<Dashboard />} />
+          {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeePollResponses) && <Route path="/responses/polls/:dataId" element={<ResponsesListPoll />} />}
+          {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeeEventResponses) && <Route path="/responses/events/:dataId" element={<ResponsesListEvent />} />}
+          {auth.accessToken && auth.permissions.includes(permissionsMapping.canApprovePendingExcuses) && <Route path="/pendingexcuses" element={<PendingExcusesPage />} />}
+          {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeeEnrolledCadets) && <Route path="/enrolled/cadets" element={<EnrolledCadets />} />}
+          {auth.accessToken && auth.permissions.includes(permissionsMapping.canSeeProbationCadets) && <Route path="/enrolled/probation" element={<ProbationCadets />} />}
+          {auth.accessToken && auth.permissions.includes(permissionsMapping.canApproveCadetAccounts) && <Route path="/approveAccounts" element={<ApproveCadetAccountPage />} />}
+          {auth.accessToken && (
+            auth.permissions.includes(permissionsMapping.canAuthorizeWingSenior) ||
+            auth.permissions.includes(permissionsMapping.canAuthorizeProbationSenior)
+          ) && <Route path="/authorize" element={<AuthorizeCadetsPage />} />}
+          {auth.accessToken && <Route path="/events" element={<EventsPage />} />}
+          {auth.accessToken && <Route path="/polls" element={<PollsPage />} />}
+          {auth.accessToken && <Route path="/notifications" element={<NotificationsPage />} />}
+          {auth.accessToken && <Route path="/account" element={<AccountPage />} />}
+          {auth.accessToken && <Route path="/settings" element={<SettingsPage />} />}
+          {auth.accessToken && <Route path="/studymaterial" element={<StudyMaterialPage />} />}
+          {auth.accessToken && <Route path="/campdetails" element={<CampDetailsPage />} />}
+        </Route>
         <Route path="*" element={<FourOFourPage />} />
       </Routes>
     </>
