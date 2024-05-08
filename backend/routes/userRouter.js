8f87540af1fdc155dd183d5d6cc002dd6915ef6c
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-08 13:04:09 +0530

    fix: fixed paths for user router and added promote and demote routes

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,12 +1,18 @@
 import changeUserPassword from "../controllers/changeUserPassword.js";
+import demoteCadet from "../controllers/demoteCadet.js";
 import express from "express";
 import getUserData from "../controllers/getUserData.js";
+import permissionsMapping from "../config/permissionsMapping.js";
+import promoteCadet from "../controllers/promoteCadet.js";
 import setUserData from "../controllers/setUserData.js";
+import verifyPermissions from "../middleware/verifyPermissions.js";
 
 const Router = express.Router();
 
-Router.get("/user", getUserData)
-Router.post("/user", setUserData)
-Router.post("/user/changePwd", changeUserPassword)
+Router.get("/", getUserData)
+Router.post("/", setUserData)
+Router.post("/changePwd", changeUserPassword)
+Router.put("/promote", verifyPermissions(permissionsMapping.canAuthorizeWingSenior, permissionsMapping.canAuthorizeProbationSenior), promoteCadet)
+Router.put("/demote", verifyPermissions(permissionsMapping.canDemoteWingSenior, permissionsMapping.canDemoteProbationSenior), demoteCadet)
 
 export default Router
