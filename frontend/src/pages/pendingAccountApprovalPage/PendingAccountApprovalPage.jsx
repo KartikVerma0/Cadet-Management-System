fcecb8e5cfb2ed77d96864b20dcc957506462aeb
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-05-08 18:20:25 +0530

    new: Initialized pending approval page

---
+++ frontend/src/pages/pendingAccountApprovalPage/PendingAccountApprovalPage.jsx
@@ -0,0 +1,31 @@
+import "./PendingAccountApprovalPage.css";
+import Lottie from "react-lottie";
+import Navbar from "../../components/navbar/Navbar";
+import WaitAnimation from "../../assets/wait_animation.json";
+
+const PendingAccountApprovalPage = () => {
+    const waitLottieOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: WaitAnimation,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    }
+    return (
+        <div className='PendingAccountApprovalPage'>
+            <Navbar loginType="logout" />
+            <main>
+                <Lottie options={waitLottieOptions}
+                    height={400}
+                    width={400}
+                />
+                <span className="message">
+                    <p>Thank you for registering on Chitkara NCC CMS.</p>
+                    <p>You will be able to use your account after approval</p>
+                </span>
+            </main>
+        </div>
+    );
+};
+export default PendingAccountApprovalPage;
